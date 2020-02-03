2c6360ac7542814e301e8a8cc431590cdda76658
John Pendenque <pendenquejohn@gmail.com>
2020-02-03 17:29:10 +0100

    Updated errors #1

--- db/errors.py
+++ db/errors.py
@@ -1,18 +1,15 @@
 class DatabaseError(Exception):
-    def __init__(self, message):
+    """Top level or global database error"""
+    def __init__(self, message, **kwargs):
         self.message = message
 
     def __str__(self):
         return self.message
 
 class SchemaError(DatabaseError):
-    pass
-
-class FieldError(DatabaseError):
-    pass
-
-class ValidatorError(DatabaseError):
-    pass
+    """Errors related to the structure of the JSON schema"""
+    def __init__(self, fields):
+        super().__init__(f'The following fields "{", ".join(fields)}" are missing from your schema.')
 
 class FilterError(DatabaseError):
     """Raises an error when a special keyword is erroneous"""
@@ -23,18 +20,29 @@ class FilterError(DatabaseError):
 class KeyExistError(DatabaseError):
     """A specific error for when a key the user tries to query does not exist"""
     def __init__(self, key, keys):
-        self.message = 'The key (%s) that you are trying to get does not exist. Available keys are: %s' % (key, keys)
+        self.message = f'The key "{key}" that you are trying to get does not exist. Available keys are: {", ".join(keys)}.'
 
 class ItemExistError(DatabaseError):
     """An error for when an item does not exist in the database"""
-    def __init__(self, reference):
-        self.message = 'The item with id "%s" does not exist in your database' % reference
+    def __init__(self):
+        self.message = 'The item you are looking for does not exist in your database'
 
 class SubDictError(DatabaseError):
+    """When the returned value is a dictionnary as opposed to a value e.g. int, float..."""
     def __init__(self, f, subdict, **kwargs):
         first_key = list(subdict.keys())[0]
-        self.message = '"%s" filter returned a subdictionnary.\
-        You should push your filter to query that. For example: %s__%s' % (f, f, first_key)
+        message = '"%s" expression returned a subdictionnary. You should push your expression to ' \
+                        'explore it further. For example: %s__%s' % (f, f, first_key)
+        super().__init__(message)
 
-    def __str__(self):
-        return self.message
+class NullFieldError(DatabaseError):
+    """Error raised when a models' field, 
+    not considered as abstract, is None"""
+    def __init__(self, model_name, field):
+        super().__init__('"%s" is None while %s is not set to be used as an abstract Model.' % field, model_name)
+
+class FieldError(DatabaseError):
+    pass
+
+class ValidatorError(DatabaseError):
+    pass
