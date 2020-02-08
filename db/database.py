54fcbf229ad13a7c7b8855bbe8b1bfcbfd4436cb
John Pendenque <pendenquejohn@gmail.com>
2020-02-08 15:22:37 +0100

    Update version of the main Database entrypoint

--- db/database.py
+++ db/database.py
@@ -3,249 +3,229 @@ import json
 import os
 import secrets
 
-from django_no_sql.db.decorators import database_cache
-from django_no_sql.db.errors import DatabaseError, SchemaError
-from django_no_sql.db.fields import CharField
-from django_no_sql.db.functions import Functions
+from django_no_sql.db.errors import (DatabaseError, FieldError,
+                                     ManagerLoadingError, PrimaryKeyError,
+                                     SchemaError)
 from django_no_sql.db.managers import Manager
-from django_no_sql.db.queryset import QuerySet
 
+PATH = 'C:\\Users\\Pende\\Documents\\myapps\\django_no_sql\\db\\database.json'
 
-class Database(Functions):
-    """This class is the main entrypoint for creating and doing
-    actions on the database.
+class Database:
+    loaded_json_data = []
+    created_from_model = False
+    database_loaded = False
 
-    The database is considered as the .json file that contain the 
-    related schema, field properties and stored data.
+    model_fields = []
+    field_names = []
 
-    Description
-    -----------
+    # Fields that can be null,
+    # empty or both
+    required_fields = {'empty': [], 'null': [], 'both': []}
 
-        There are many ways to create a database. The first one is to
-        use the database instance as below:
-
-            database = Database(path_or_url=path_to_file)
-
-        The second one is by subclassing the Model class:
-
-            class Celebrity(Model):
-                pass
-
-        If you decide to create the database inline, you need to provide
-        the fields, model name [...] exactly like you would for subclassing
-        the Model class.
-
-    Parameters
-    ----------
-
-        path_or_url: is the path or the url of the schema/json that you wish to use
-
-        fields: are the fields that you want to create if the database does not exist
-
-        types: are the types of each fields
-
-            This process can be facilitated by using the Fields classes such as
-            IntegerField, CharField etc.
-    """
-    def __init__(self, path_or_url=None, **kwargs):
+    def __init__(self, path_or_url=None):
         self.path_or_url = path_or_url
-        self.kwargs = kwargs
 
-        if self.path_or_url:
-            path_exists = os.path.exists(path_or_url)
-            if not path_exists:
-                pass
+    def __getattr__(self, name):
+        # When we try to use the manager
+        # without loading the database
+        # before hand, we cannot access
+        # the manager property. In which
+        # case we need to raise a more
+        # convinient error
+        if name == 'manager':
+            if not self.database_loaded:
+                raise ManagerLoadingError()
+        return name
+
+    def load_database(self):
+        """Opens the local database and automatically 
+        calls the .check_schema()"""
+        with open(self.path_or_url, 'r', encoding='utf-8') as db:
+            raw_data = json.load(db)
+            self.database_loaded = True
+            self.loaded_json_data = self.transform_data(raw_data)
+            self.set_database_class(data_to_use=raw_data)
+        return raw_data
+
+    def set_database_class(self, data_to_use=None, **kwargs):
+        """Defines the the different properties of the class
+        
+        Properties
+        ----------
+
+            Data_to_use: The data of the current JSON file to
+            use to initialize the Manager and QuerySet
+        """
+        field_errors = []
+
+        if 'title' in data_to_use:
+            self.model_name = data_to_use['title']
+        else:
+            field_errors.append('title')
+
+        if 'properties' in data_to_use:
+            properties = data_to_use['properties']
+            if properties and not self.model_fields and not self.field_names:
+                self.model_fields = [prop for prop in properties.values()]
+                self.field_names = properties.keys()
+        else:
+            field_errors.append('properties')
+        
+        # Map the fields that require a value and
+        # cannot be empty, null or both
+        for field in self.model_fields:
+            if field.cannot_be_null:
+                self.required_fields['null'].append(field)
+            elif field.cannot_be_empty:
+                self.required_fields['empty'].append(field)
+            elif not field.cannot_be_null and not field.cannot_be_empty:
+                self.required_fields['both'].append(field)
+
+        if not self.created_from_model and self.database_loaded:
+            self.manager = Manager(db_instance=self)
+
+        if field_errors:
+            raise DatabaseError('%s fields are not present in your database.' % field_errors)
+    
+        return True
+    
+    def _update_schema(self, schema, field, value):
+        """Updates specific fields of the database schema"""
+        allowed_fields = ['title', 'version', 'properties']
+        if field not in allowed_fields:
+            pass
+        with open(self.path_or_url, 'w', encoding='utf-8') as db:
+            try:
+                schema[field] = value
+            except KeyError:
+                raise
             else:
-                db_data = self.load_database(path_or_url)
+                schema['version'] = schema['version'] + 1
+                schema['modified_on'] = str(datetime.datetime.now())
+                json.dump(schema, db, indent=4)
+        return True
 
-            self.db_name = os.path.basename(path_or_url)
-            # Initializes the database data within
-            # the Functions() class
-            self.db_data = db_data['data']
-            # Manager for interracting with the database
-            self.manager = Manager(data=self.db_data)
-
-    def create(self, name, fields:list, models:list=None, **kwargs):
-        """A definition that can create a database outside of
-        of a class based structure.
+    def transform_data(self, raw_data, key=None):
+        """Transforms the data from the database into an list
+        containing a series of dictionnaries for manageability.
 
         Description
         -----------
 
-            Suppose we want to create a new database and return the
-            handle (cursor) of the latter. We can do so by using:
+            In order to simplify iteration over the top dictionnary,
+            storing each dict as an array does exactly that
 
         Parameters
         ----------
 
-            Fields: they can be either string values or Field classes that
-            we be converted accordingly for the database
-        """
-        # if self.path_or_url:
-        #     raise DatabaseError('You are trying to both load and create a database at the same time.' 
-        #         ' If you wish to create a new database, do not use path_or_url')
-        # print(name, fields, models)
-        pass
-
-    def migrate(self, **kwargs):
-        """Creates a new version of the database by changing the
-        modified_on and increasing the version of the schema
-        """
-        pass
-    
-    def pre_save(self, func=None):
-        """A decorator that performs a set of actions before
-        saving data to the database
-        """
-        return self.save(data=func(self.db_data))
-
-    def post_save(self, func=None):
-        """A decorator that performs a set of actions after
-        saving data to the database
+            Key: The key of the data field in your database. Generally
+            the name is "data" but you can override it here
 
         Example
         -------
 
+            {1: {a: b}, 2: {c: d}} becomes [{a: b}, {c: d}]
+        """ 
+        if isinstance(self.db_data, list):
+            return self.db_data
+        if not key:
+            key = 'data'
+        return [record for record in raw_data[key].values()]
+
+    def create_from_model(self, model_name, fields_to_create:list, **kwargs):
+        """Create a database from a set of models"""
+        if not os.path.exists(self.path_or_url):
+            # Creates the database here
+            with open(self.path_or_url, 'w', encoding='utf-8') as db:
+                schema_structure = {
+                    "$schema": "http://json-schema.org/draft-07/schema",
+                    "$id": secrets.token_hex(nbytes=25),
+                    "title": model_name,
+                    "created_on": datetime.datetime.now().timestamp(),
+                    "modified_on": "",
+                    "version": 1,
+                    "data": {},
+                    "properties": {}
+                }
+                schema_structure = self._create_fields(schema_structure, fields_to_create)
+                json.dump(schema_structure, db, indent=4)
+            return True
+        else:
+            # Otherwise, just load the database 
+            # and return its data
+            raw_data = self.load_database()
+            # Check that the title of the model corresponds
+            # the current Model class that is trying to
+            # access the database model
+            # if all_data['title'] == model_name:
+            #     raise Exception('The current model does not exist in the database')
+            return self.database_loaded
+
     @staticmethod
-    def _create_fields(data:dict, fields:list):
+    def _create_fields(schema:dict, fields:list):
         """A definition that creates new properties fields in the JSON schema"""
         new_fields = {field.as_dict['name']: field.as_dict for field in fields}
-        data.update({'properties': new_fields})
-        return data
-        """
-        saved_data = self.save(data=func(self.db_data))
-        return saved_data
+        schema.update({'properties': new_fields})
+        return schema
 
-    def pre_delete(self):
-        pass
+    def _compare_fields(self, schema, model_fields:list):
+        """Checks if the current fields from the database and the incoming
+        fields from the model are the same. If not, it updates the
+        model fields"""
+        # Current properties are field classes,
+        # we have to render them as dictionnaries
+        current_database_fields = [prop for prop in schema['properties'].values()]
+        model_fields = [model_field.as_dict for model_field in model_fields]
+        if not current_database_fields == model_fields:
+            new_model_fields = {model_field['name']: model_field for model_field in model_fields}
+            self._update_schema(schema, 'properties', new_model_fields)
+            return True
+        return False
 
-    def post_delete(self):
-        pass
+    def migrate(self):
+        loaded_data = self.load_database()
+        print('Migration complete!')
+        return self._compare_fields(loaded_data, self.model_fields)
 
-    @classmethod
-    def save(cls, data=None, **kwargs):
-        """A definition that commits changes to the database
-        by writting objects into it
-        """
-        # If data is None, then
-        # just return the cached_version
-        # of the database to the user
-        if data is None:
-            return ''
+    # @classmethod
+    # def prepare_save(cls, data_to_save:dict=None):
+    #     """Prepares the database and the data for saving before
+    #     committing to the actual database
 
-        # If data is passed but is not a dict,
-        # then directly raise an exception here
-        if data and not isinstance(data, dict):
-            raise Exception()
-        else:
-            # cls.clean(data=data)
+    #     Description
+    #     -----------
 
-            # Otherwise we can open the database in order
-            # to commit the new changes
-            with open(PATH, 'w', encoding='utf-8') as f:
-                data['$modified_on'] = cls.set_date()
-                json.dump(data , f, indent=4, sort_keys=True)
-            return cls
+    #     """
+    #     key, values = data_to_save.items()
+    #     if not isinstance(key, int):
+    #         raise PrimaryKeyError(key)
 
-    def load_database(self, path_or_url):
-        """A simple function that opens the local database
-        to retrieve all the data that it contains
-        """
-        with open(path_or_url, 'r', encoding='utf-8') as f:
-            data = json.load(f)
-        return self.check_schema(data)
+    #     errors = []
+    #     for key in values.keys():
+    #         if key not cls.field_names:
+    #             errors.append('%s is not a field of your model. Model fields are: ' % (key, ', '.join(cls.model_fields)))
+    #             raise FieldError(errors)
 
-    def refresh_from_database(self):
-        pass
-    
-    def check_kwargs(self, **kwargs):
-        return kwargs
+    #     # Here we verify the type of the
+    #     # values that want to passed to
+    #     # the database in respect to the
+    #     # properties stored
+    #     validates = []
+    #     for field in cls.model_fields:
+    #         pass
+        
+    #     def commit(state=True):
+    #         return data_to_save
 
-    def check_schema(self, data):
-        """This function is called everytime the database is loaded in order
-        to check for the integrity of the database we are going to work with.
+    #     return commit
 
-        Description
-        -----------
-
-            It checks of the presence of the following fields:
-                - iD
-                - Created_on
-                - Version
-
-            The presence of certain fields can raise an error or not
-        """
-        if '$id' not in data:
-            raise SchemaError('We could not find the required key $id in the schema of your database')
-        else:
-            # Here we create a valid identifier for the
-            # the databaase if it is none or blank
-            if data['$id'] is None or data['$id'] == '':
-                data['$id'] = secrets.token_hex(nbytes=25)
-
-        # Require a created_on date so that we
-        # can also keep track of the version of
-        # the database
-        if '$created_on' not in data:
-            data['$created_on'] = self.set_date()
-        else:
-            if data['$created_on'] == None or data['$created_on'] == '':
-                data['$created_on'] = self.set_date()
-
-        if '$version' not in data:
-            data['$version'] = 0
-        else:
-            if data['$version'] == None or data['$version'] == '':
-                data['$created_on'] = 0
-        return data
-    
-    @staticmethod
-    def set_date():
-        """Get the current date as a timestamp"""
-        return datetime.datetime.now().timestamp()
-
-    @staticmethod
-    def calculate_version(n):
-        """Bump the current database version by one"""
-        return n + 1
-
-    def clean(self, **kwargs):
-        """This definition is called just before data is committed (or written)
-        to the database in order to prevent corruption.
-        """
-        pass
-    
-class Models(type):
-    """Models are a way of structuring the logic of your database
-    efficiently using class methods
-
-    Description
-    -----------
-
-        class FashionModels(Models):
-            pass
-    """
-    def __new__(cls, name, bases, cls_dict):
-        new_class = super().__new__(cls, name, bases, cls_dict)
-        if name != 'A':
-            model_name = name
-            fields_to_create = []
-            # Get each fields from the database in order
-            # to be able to create the model
-            for key, value in cls_dict.items():
-                if isinstance(value, CharField):
-                    item = value.as_dict
-                    item['name'] = key
-                    fields_to_create.append(item)
-
-                    # Use a database instance to create
-                    # all the models
-                    database = Database()
-                    database.create(model_name, [], fields_to_create)
-                    # Implement the instance to all models
-                    setattr(new_class, 'database', database)
-        return new_class
-
-class A(metaclass=Models):
-    def save(self, data=None):
-        pass
+class LinkedDatabase(Database):
+    """Create databases that are linked to one another
+    by a one-to-many or one-to-one links"""
+    inner_link = 'one-to-many'
+    related_databases = []
+    # This is the many dictionnary that
+    # remembers the links between each
+    # database e.g. {a: {type: onetomany, databases: [b, c]}}
+    database_links = {}
+    prefetch = True
