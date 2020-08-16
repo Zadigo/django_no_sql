27fa7a3e3132b728b148dc7799b96e172a1b55d8
John Pendenque <pendenquejohn@gmail.com>
2020-08-16 23:06:00 +0200

    Rebuild cache code

--- db/decorators.py
+++ db/decorators.py
@@ -1,8 +1,22 @@
+from django_no_sql.db import errors
+
 def database_cache(func):
-    def cached_data(self, path_or_url):
-        """Opens the database and returns a cached version
-        of the data that it contains
-        """
-        cached_data = func(self, path_or_url)
-        return cached_data
-    return cached_data
+    """A cache function that stores the data coming from the
+    database and returns a dictionnary.
+
+    Result
+    ------
+
+        {schema: '', data: '', id: ''}
+    
+    Parameters
+    ----------
+        
+        path: current path to the database
+        
+        key: the main key entrance to access the database data
+    """
+    pass
+
+def database_cache_for_web(func):
+    pass
